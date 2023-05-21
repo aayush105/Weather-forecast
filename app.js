4081683b85849e10077b87238d6340df742298e0
Aayush shrestha <aayushrestha5@gmail.com>
2023-05-21 18:46:45 +0545

    Any Location's weather can be checked

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const { write } = require("fs");
+// const { write } = require("fs");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Nepal&appid=a63439b3be41ca1d0f4c92c903d8ecf5&units=metric"  // api of the weather website
+app.get("/", function(req, res){
+    res.sendFile(__dirname+ "/index.html");
+});
+
+app.post("/", function(req, res){
+    // console.log(req.body.cityName); // gives the output of the country inputed in the website
+
+    const query=req.body.cityName;
+    const apiKey="a63439b3be41ca1d0f4c92c903d8ecf5"; 
+    const unit="metric"; // degree of temp (celcius)
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit ; // api of the weather website
 
     https.get(url, function(response){
         console.log(response.statusCode); // will provide the status code of the url in hyper terminal 
@@ -28,7 +39,7 @@ app.get("/", function(req, res){
 
             res.write("<p>The weather is currently " + weatherDescription +"</p>" ); // to show on our website
 
-            res.write("<h1>The temperature in Nepal is "+ temp +" degrees celcius.</h1>"); // only one res.send can be used so we used res.write for multiple times and then used res.send once to send our info to the website
+            res.write("<h1>The temperature in " + query + " is "+ temp +" degrees Celcius.</h1>"); // only one res.send can be used so we used res.write for multiple times and then used res.send once to send our info to the website
 
             res.write("<img src="+ imageURL +">");
             res.send();
@@ -41,7 +52,9 @@ app.get("/", function(req, res){
             */
         })
     })
+    
 })
+
 
 app.listen(3000, function(){
     console.log("Server is running at port 3000.");
